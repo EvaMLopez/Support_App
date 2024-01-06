1e55d2d36347bb7c31a355548f990793695f78fa
Eva López <evamlopez1981@gmail.com>
2024-01-06 18:53:00 +0100

    merge tickets-view with dev

--- backend/src/main/java/dev/evalopez/appsupport/AppsupportApplication.java
+++ backend/src/main/java/dev/evalopez/appsupport/AppsupportApplication.java
@@ -3,7 +3,9 @@ package dev.evalopez.appsupport;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "http://localhost:5173") 
 @SpringBootApplication
 @ComponentScan("dev.evalopez.appsupport")
 public class AppsupportApplication {
